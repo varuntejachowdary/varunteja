7ab2ee9f488d25a3f67ec4663b010beb17203feb
varuntejachowdary <36836188+varuntejachowdary@users.noreply.github.com>
2018-02-26 13:10:50 +0530

    Update varun.py

--- varun.py
+++ varun.py
@@ -6,10 +6,10 @@ app=Flask(_name_)
 
 @app.route("/")
 daf hello():
- 	return jsonifly({
+ return jsonifly({
  	  'message':'vineeth likes keyboards',
  	  'flag':True
  	})
 
 if_name_=="_main_":
-    	app.run(debug=True)
+ app.run(debug=True)
