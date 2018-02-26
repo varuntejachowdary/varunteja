b3a90dc862f769dbf10a2fc8bede5f9f846de1a9
varuntejachowdary <36836188+varuntejachowdary@users.noreply.github.com>
2018-02-26 12:32:06 +0530

    Add files via upload

---
+++ varun.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+
+
+app=Flask(_name_)
+
+@app.route("/")
+daf hello():
+ 	return jsonifly({
+ 	  'message':'vineeth likes keyboards',
+ 	  'flag':True
+ 	})
+
+if_name_=="_main_":
+    	app.run(debug=True)
